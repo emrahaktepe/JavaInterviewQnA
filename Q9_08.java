4411ed0a565a6ecfbee6478aea7e0f60ea572a63
Emrah Aktepe <emrahaktepe@gmail.com>
2020-01-20 23:38:52 -0600

    9_08

--- Q9_08.java
+++ Q9_08.java
@@ -6,5 +6,28 @@ public class Q9_08 {
 //		 turn each word into char array print reverse of each char out with 
 //		 seperated by - output : k-e-t-r-e-b-y-C 	...
 
+	public static void main(String[] args) {
+		String str = "Cybertek Batch Spartan is most hardworking Batch ever";
+		
+		String[] arr = str.split(" ");
+		for (int i = 0; i<arr.length; i++) {
+			char[] ch = arr[i].toCharArray();
+			ch = reverseChar(ch);
+			System.out.println(ch);
+		}
+		
+	}
+	
+	public static char[] reverseChar(char[] ch){	
+		char[] chNew = new char[ch.length];
+		int j = 0;
+		
+	for(int i = ch.length-1; i >= 0; i--) {		
+		chNew[j]=ch[i];
+		j++;
+	}	
+	return chNew;
+
+}
 	
 }
